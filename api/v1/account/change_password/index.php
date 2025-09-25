f13cb9043f8c2572bf8239bba77ac1e15f3bd0ce
VmawaV <desarrollo.1@net2alliance.com>
2025-09-25 18:40:00 +0000

    feature(session_handling): recovery and change_password endpoints

---
+++ api/v1/account/change_password/index.php
@@ -0,0 +1,116 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(['error' => 'The requested resource does not support HTTP method ' . $_SERVER['REQUEST_METHOD']]);
+    exit;
+}
+$headers = getallheaders();
+if ((!isset($_POST["recovery_token"]) && !isset($headers['Authorization'])) || !isset($_POST["new_password"])) {
+    http_response_code(401);
+    echo json_encode(['error' => 'Missing parameters']);
+    exit;
+}
+require_once __DIR__ . "/../../../../config/cors.php";
+require_once __DIR__ . "/../../../../functions/serverSpecifics.php";
+require_once __DIR__ . "/../../../../utils/token/validate.php";
+require_once __DIR__ . "/../../../../utils/token/delete.php";
+require_once __DIR__ . "/../../../../utils/mail/send.php";
+
+$ProductionLibServerSpecifics = ServerSpecifics::getInstance();
+$server_url = $ProductionLibServerSpecifics->fnt_getServerURL();
+$DBLINK = $ProductionLibServerSpecifics->fnt_getDBConnection();
+$new_password = $_POST['new_password'];
+
+
+if (isset($_POST["recovery_token"])) {
+    $recovery_token = $_POST["recovery_token"];
+    $validation = fnt_validateRecoveryPasswordToken_v001($DBLINK, $recovery_token);
+    if ($validation === false) {
+        http_response_code(401);
+        header('Content-Type: application/json');
+        echo json_encode(array("error" => "Invalid or expired token"));
+        exit;
+    }
+    $acco_id = $validation['user_id'];
+    $acco_email = $validation['user_email'];
+} else {
+    $auth_header = $headers['Authorization'];
+    $data = explode(' ', $auth_header);
+    if (count($data) !== 2) {
+        http_response_code(401);
+        header('Content-Type: application/json');
+        echo json_encode(array("error" => "Invalid or expired token"));
+        exit;
+    }
+    $auth_method = $data[0];
+    if($auth_method !== "Bearer"){
+        http_response_code(401);
+        header('Content-Type: application/json');
+        echo json_encode(array("error" => "Invalid or expired token"));
+        exit;
+    }
+    $jwt = $data[1];
+    $decoded_jwt = fnt_validateJWT_v001($jwt);
+    if ($decoded_jwt === false) {
+        http_response_code(401);
+        header('Content-Type: application/json');
+        echo json_encode(array("error" => "Invalid or expired token"));
+        exit;
+    }
+    $acco_id = $decoded_jwt['user_id'];
+    $acco_email = $decoded_jwt['user_email'];
+}
+
+mysqli_begin_transaction($DBLINK);
+
+$hashed_new_password = hash('sha256', $new_password);
+
+$qry_updatePassword = "
+        UPDATE account
+        SET acco_password = ?
+        WHERE acco_id = ?
+    ";
+$stmt = mysqli_prepare($DBLINK, $qry_updatePassword);
+
+if ($stmt === false) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Error while updating password']);
+    mysqli_rollback($DBLINK);
+    exit;
+}
+
+mysqli_stmt_bind_param($stmt, 'si', $hashed_new_password, $param_userID);
+if(!mysqli_stmt_execute($stmt)){
+    mysqli_stmt_close($stmt);
+    http_response_code(500);
+    mysqli_rollback($DBLINK);
+    echo json_encode(['error' => 'Error while updating password']);
+    exit;
+}
+mysqli_stmt_close($stmt);
+
+
+if (!fnt_sendEmailForChangedPassword_v001($acco_email,$server_url)){
+    http_response_code(500);
+    echo json_encode(['error' => 'Error while sending mail']);
+    mysqli_stmt_close($stmt);
+    exit;
+}
+
+
+if (isset($_POST["recovery_token"])) {
+    $recovery_token = $_POST["recovery_token"];
+    if(!fnt_deleteRecoveryPasswordToken_v001($DBLINK,$recovery_token)){
+        mysqli_rollback($DBLINK);
+        http_response_code(500);
+        echo json_encode(['error' => 'Error while deleting token']);
+        exit;
+    }
+}
+
+mysqli_query($DBLINK, 'COMMIT;');
+echo json_encode(['valid' => 'password_updated']);
+http_response_code(200);
+
+
+
---
+++ api/v1/account/recovery/index.php
@@ -0,0 +1,63 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(['error' => 'The requested resource does not support HTTP method ' . $_SERVER['REQUEST_METHOD']]);
+    exit;
+}
+
+if (!isset($_POST['acco_email'])) {
+    http_response_code(401);
+    echo json_encode(['error' => 'Missing parameters']);
+    exit;
+}
+
+require_once __DIR__ . "/../../../../config/cors.php";
+require_once __DIR__ . "/../../../../functions/serverSpecifics.php";
+require_once __DIR__ . "/../../../../utils/token/create.php";
+require_once __DIR__ . "/../../../../utils/token/storage.php";
+require_once __DIR__ . "/../../../../utils/mail/send.php";
+
+$ProductionLibServerSpecifics = ServerSpecifics::getInstance();
+$server_url = $ProductionLibServerSpecifics->fnt_getServerURL();
+$DBLINK = $ProductionLibServerSpecifics->fnt_getDBConnection();
+$acco_email = $_POST["acco_email"];
+
+
+$qry_verifyToken = "SELECT * FROM pending_token WHERE acco_email = ? AND type = 'PASSWORD'";
+$stmt_checkToken = mysqli_prepare($DBLINK, $qry_verifyToken);
+
+mysqli_stmt_bind_param($stmt_checkToken, 's', $acco_email);
+mysqli_stmt_execute($stmt_checkToken);
+mysqli_stmt_store_result($stmt_checkToken);
+if (mysqli_stmt_num_rows($stmt_checkToken) > 0) {
+    http_response_code(409);
+    echo json_encode(['error' => 'Token already made']);
+    mysqli_stmt_close($stmt_checkToken);
+    exit;
+}
+
+$token  = fnt_createEncryptedToken_v001($acco_email, 3600);
+$encoded_token = urlencode($token);
+
+if(!fnt_insertRecoveryPasswordToken_v001($DBLINK,$acco_email,$token,3600)){
+    http_response_code(500);
+    echo json_encode(['error' => 'Error while saving token']);
+    mysqli_stmt_close($stmt_checkToken);
+    exit;
+}
+
+if (!fnt_sendEmailForRecoveryPassword_v001($acco_email,$encoded_token,$server_url)){
+    http_response_code(500);
+    echo json_encode(['error' => 'Error while sending mail']);
+    mysqli_stmt_close($stmt_checkToken);
+    exit;
+}
+
+http_response_code(200);
+echo json_encode(['valid' => ['recovery_token'=>$token]]);
+mysqli_stmt_close($stmt_checkToken);
+exit;
